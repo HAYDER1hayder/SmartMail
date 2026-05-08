73aaeebb4b3772213654d5d8355ebd7979f44758
HAYDER1hayder <kayshayder30@gmail.com>
2026-05-08 22:44:38 +0100

    Cancle Header and add Id

--- app/src/main/java/com/example/smartmail/data/remote/SmartMailApi.kt
+++ app/src/main/java/com/example/smartmail/data/remote/SmartMailApi.kt
@@ -1,16 +1,25 @@
 package com.example.smartmail.data.remote
 
 import com.example.smartmail.data.local.SmartMailEntity
+import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Header
+import retrofit2.http.Query
 
+// هذه الواجهة هي عقد الاتفاق بيننا وبين Spring Boot (الذي صنعه صديقك)
 interface SmartMailApi {
 
-    // هنا سنضع رابط صديقك لاحقاً (مثلاً: /api/v1/emails)
-    @GET("TODO_YOUR_FRIEND_ENDPOINT_HERE")
+    // 1. جلب الإيميلات الجديدة
+    @GET("/api/v1/emails/list")
     suspend fun fetchSmartMails(
-        // 👇 هذا هو مفتاح الأمان الذي يطلبه Spring Boot
-        @Header("Authorization") token: String
+        @Query("user_id") userId: String
     ): List<SmartMailEntity>
+
+    // 2. بوابة التوثيق (Authentication Gateway)
+    // نرسل للسيرفر الكود السري الخاص بجوجل ليسمح لنا بالدخول
+    @GET("/api/v1/auth/google")
+    suspend fun authenticateWithBackend(
+        @Query("code") serverAuthCode: String,
+        @Query("uid") firebaseUid: String
+    ): Response<String>
 
 }
